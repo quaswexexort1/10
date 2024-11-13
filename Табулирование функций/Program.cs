d3a31a49ace9c86436bfb36a1e41f09e7fae97c4
SHEL <SHEL@DESKTOP-6SFOS1M>
2024-11-13 23:18:13 +0200

    Добавьте файлы проекта.

---
+++ Табулирование функций/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//ИНТЕРВАЛ [2;4]
+double s = 2;
+double e = 4;
+
+//ШАГ ТАБУЛИРОВАНИЯ
+double step = 0.2;
+
+Console.WriteLine("x     |     y");
+Console.WriteLine("-------------");
+
+for (double x = s; x <= e; x += step)
+{
+    double y = CF(x);
+    Console.WriteLine($"{x:F2} | {y:F2}");
+}
+
+// Функция для вычисления значения функции y = x^3 + 3x^2 - 3
+static double CF(double x)
+{
+    return Math.Pow(x, 3) + 3 * Math.Pow(x, 2) - 3;
+}
+
